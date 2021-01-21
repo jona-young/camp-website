b7c87a8fb1ad375083d5b918606562f0e6b4805e
jonayoung <young.jon@icloud.com>
2021-01-21 01:23:54 -0500

    Updates

---
+++ precache-manifest.ad7d047b7f9b1b924ca433077789e048.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644d404b9f3de55dd40061c34701ae75",
+    "url": "/camp-website/index.html"
+  },
+  {
+    "revision": "2fec914c4763864c17fc",
+    "url": "/camp-website/static/css/main.6d4f2a13.chunk.css"
+  },
+  {
+    "revision": "c287476169a862ddfa6b",
+    "url": "/camp-website/static/js/2.4fbac5d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/camp-website/static/js/2.4fbac5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fec914c4763864c17fc",
+    "url": "/camp-website/static/js/main.a8d9f4b6.chunk.js"
+  },
+  {
+    "revision": "28c6f77be830d5ec48b2",
+    "url": "/camp-website/static/js/runtime-main.9b015a06.js"
+  },
+  {
+    "revision": "fbe8471e5a4821c0dd3494f37bc69f46",
+    "url": "/camp-website/static/media/badminton.fbe8471e.jpg"
+  },
+  {
+    "revision": "adb024ed7ec4061da648e6cd4d38671c",
+    "url": "/camp-website/static/media/camp.adb024ed.jpg"
+  },
+  {
+    "revision": "7bae0cd78a366dbb1844fa23c9263b29",
+    "url": "/camp-website/static/media/icon.7bae0cd7.jpg"
+  },
+  {
+    "revision": "fddc1b6c627d43f97b138428f86697bd",
+    "url": "/camp-website/static/media/info-cover.fddc1b6c.jpg"
+  },
+  {
+    "revision": "827d9943d7eb091a89cd0691e5932ab2",
+    "url": "/camp-website/static/media/kids-cover.827d9943.jpg"
+  },
+  {
+    "revision": "90c74a243f1d876921b4e327bb076b79",
+    "url": "/camp-website/static/media/squash.90c74a24.jpg"
+  },
+  {
+    "revision": "920cdb35ffd7a5cdd51ac2a8a69defcc",
+    "url": "/camp-website/static/media/tennis.920cdb35.jpg"
+  }
+]);
